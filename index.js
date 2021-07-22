3a8c2b9a1ec1d7356ce2b72c547445d882ea7226
Yash <64688647+Yash-Kr@users.noreply.github.com>
2021-07-22 15:54:19 +0530

    added console.log() statements

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ $(document).ready(function(){
   {
 
     let daa = $('#daa option:selected').val();
+    console.log('daa = "+daa);
     let os = $('#os option:selected').val();
+    console.log('os = "+os);
     let flat = $('#flat option:selected').val();
+    console.log('flat = "+flat);
     let ca = $('#ca option:selected').val();
+    console.log('ca = "+ca);
     let oosd = $('#oosd option:selected').val();
+    console.log('oosd = "+oosd);
     let moocs = $('#moocs option:selected').val();
+    console.log('moocs = "+moocs);
     let oslab = $('#oslab option:selected').val();
+    console.log('oslab = "+oslab);
     let algolab = $('#algolab option:selected').val();
+    console.log('algolab = "+algolab);
 
     let total_points = 4*daa + 4*os + 4*flat + 3*ca + 3*oosd + 3*moocs + 1*oslab + 1*algolab ;
     let sgpa = total_points/23;
